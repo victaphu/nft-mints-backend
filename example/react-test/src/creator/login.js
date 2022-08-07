c5d549950b7632845661ada883fe47e5d1d53fce
Victa Kwok Wai Phu <victa.phu@gmail.com>
2022-08-08 02:11:07 +1000

    update: change to smsnftgateway2

--- example/react-test/src/creator/login.js
+++ example/react-test/src/creator/login.js
@@ -3,8 +3,8 @@ import {useState} from 'react'
 import Cookies from 'js-cookie'
 import {useNavigate} from 'react-router'
 
-// const GATEWAY = 'https://smsnftgateway.herokuapp.com'
-const GATEWAY = 'http://localhost:3000'
+const GATEWAY = 'https://smsnftgateway2.herokuapp.com'
+// const GATEWAY = 'http://localhost:3000'
 function Login() {
   const [mobileNumber, setMobileNumber] = useState('+6584901105')
   const [connecting, setConnecting] = useState(false)
