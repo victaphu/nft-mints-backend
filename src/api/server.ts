9146d7061234da821cf45c164912d40af4d30ebf
victa <victa.phu@gmail.com>
2022-08-09 09:03:17 +1000

    update: heroku does forwarding, check the x-forwarded-proto

--- src/api/server.ts
+++ src/api/server.ts
@@ -30,7 +30,7 @@ const whitelist = process.env.WHITELIST_CORS!.split(';;')
 // https://github.com/expressjs/session/issues/725
 function when(test: any, a: any, b: any) {
   return (req: any, res: any, next: any) => {
-    console.log('testing protocol', req.protocol)
+    console.log('testing protocol', req.protocol, req.headers['x-forwarded-proto'])
     return (test(req, res) ? a : b)(req, res, next)
   }
 }
@@ -57,7 +57,7 @@ export const RESTServer = async () => {
   api.use(cookieParser())
   api.use(
     when(
-      (req: any) => req.protocol === 'https',
+      (req: any) => req.headers['x-forwarded-proto'] === 'https' || req.protocol === 'https',
       session({
         secret: process.env.SESSION_SECRET!,
         cookie: {
